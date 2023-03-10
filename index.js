8dd91f8e14ef8621034448c2c1f38ff04c978a32
DorothyAriana <arianadottie@gmail.com>
2023-03-10 22:40:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+  //console.log(`I'm ${age} years old. Happy birthday to me!`);
+  //debugger;
+//}
+
+/* const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); */
+
+function writeCards (name, event) {
+    const message = [];
+    for (let i = 0; i < name.length; i++) {
+        const messages = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        message.push(messages)
+    }
+    return message;
+}
+
+function countDown (counting) {
+    while (counting > 0) {
+        console.log(counting);
+        counting -= 1;
+    }
+    console.log(counting);
+}
